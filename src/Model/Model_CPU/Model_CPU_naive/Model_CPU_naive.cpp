e941ea609a742b6598408e0129869bdd40affc1e
Andy Mery <andy.mery@imt-atlantique.net>
2021-02-26 14:26:20 +0100

    Removed unecesseray force computation

--- src/Model/Model_CPU/Model_CPU_naive/Model_CPU_naive.cpp
+++ src/Model/Model_CPU/Model_CPU_naive/Model_CPU_naive.cpp
@@ -2,14 +2,12 @@
 
 #include "Model_CPU_naive.hpp"
 
-Model_CPU_naive
-::Model_CPU_naive(const Initstate& initstate, Particles& particles)
-: Model_CPU(initstate, particles)
+Model_CPU_naive ::Model_CPU_naive(const Initstate &initstate, Particles &particles)
+	: Model_CPU(initstate, particles)
 {
 }
 
-void Model_CPU_naive
-::step()
+void Model_CPU_naive ::step()
 {
 	std::fill(accelerationsx.begin(), accelerationsx.end(), 0);
 	std::fill(accelerationsy.begin(), accelerationsy.end(), 0);
@@ -17,30 +15,32 @@ void Model_CPU_naive
 
 	for (int i = 0; i < n_particles; i++)
 	{
-		for (int j = 0; j < n_particles; j++)
+		for (int j = 0; j < i; j++)
 		{
-			if(i != j)
+
+			const float diffx = particles.x[j] - particles.x[i];
+			const float diffy = particles.y[j] - particles.y[i];
+			const float diffz = particles.z[j] - particles.z[i];
+
+			float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+			if (dij < 1.0f)
 			{
-				const float diffx = particles.x[j] - particles.x[i];
-				const float diffy = particles.y[j] - particles.y[i];
-				const float diffz = particles.z[j] - particles.z[i];
-
-				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
-
-				if (dij < 1.0)
-				{
-					dij = 10.0;
-				}
-				else
-				{
-					dij = std::sqrt(dij);
-					dij = 10.0 / (dij * dij * dij);
-				}
-
-				accelerationsx[i] += diffx * dij * initstate.masses[j];
-				accelerationsy[i] += diffy * dij * initstate.masses[j];
-				accelerationsz[i] += diffz * dij * initstate.masses[j];
+				dij = 10.0f;
 			}
+			else
+			{
+				dij = std::sqrt(dij);
+				dij = 10.0f / (dij * dij * dij);
+			}
+
+			accelerationsx[i] += diffx * dij * initstate.masses[j];
+			accelerationsy[i] += diffy * dij * initstate.masses[j];
+			accelerationsz[i] += diffz * dij * initstate.masses[j];
+
+			accelerationsx[j] -= diffx * dij * initstate.masses[i];
+			accelerationsy[j] -= diffy * dij * initstate.masses[i];
+			accelerationsz[j] -= diffz * dij * initstate.masses[i];
 		}
 	}
 
@@ -49,8 +49,8 @@ void Model_CPU_naive
 		velocitiesx[i] += accelerationsx[i] * 2.0f;
 		velocitiesy[i] += accelerationsy[i] * 2.0f;
 		velocitiesz[i] += accelerationsz[i] * 2.0f;
-		particles.x[i] += velocitiesx   [i] * 0.1f;
-		particles.y[i] += velocitiesy   [i] * 0.1f;
-		particles.z[i] += velocitiesz   [i] * 0.1f;
+		particles.x[i] += velocitiesx[i] * 0.1f;
+		particles.y[i] += velocitiesy[i] * 0.1f;
+		particles.z[i] += velocitiesz[i] * 0.1f;
 	}
 }

a262b6c9101a98ef13b84e2c92a5cc13aca5a62f
Quentin PUBERT <quentin.pubert@imt-atlantique.net>
2021-03-06 17:27:24 +0100

    2 loops "rectangle" optimization

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -20,11 +20,10 @@ void Model_CPU_fast ::step()
 
 // OMP  version
 #pragma omp parallel for
-    for (int i = 0; i < n_particles; i++)
+    for (int i = 0; i < n_particles / 2; i++)
     {
-        for (int j = i + 1; j < n_particles; j++)
+        for (int j = 0; j < i; j++)
         {
-
             const float diffx = particles.x[j] - particles.x[i];
             const float diffy = particles.y[j] - particles.y[i];
             const float diffz = particles.z[j] - particles.z[i];
@@ -49,11 +48,39 @@ void Model_CPU_fast ::step()
             accelerationsy[j] -= diffy * dij * initstate.masses[i];
             accelerationsz[j] -= diffz * dij * initstate.masses[i];
         }
-//     }
 
-// #pragma omp parallel for
-//     for (int i = 0; i < n_particles; i++)
-//     {
+        auto invi = n_particles - 1 - i;
+        for (int j = 0; j < invi; j++)
+        {
+            const float diffx = particles.x[j] - particles.x[invi];
+            const float diffy = particles.y[j] - particles.y[invi];
+            const float diffz = particles.z[j] - particles.z[invi];
+
+            float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+            if (dij < 1.0f)
+            {
+                dij = 10.0f;
+            }
+            else
+            {
+                dij = std::sqrt(dij);
+                dij = 10.0f / (dij * dij * dij);
+            }
+
+            accelerationsx[invi] += diffx * dij * initstate.masses[j];
+            accelerationsy[invi] += diffy * dij * initstate.masses[j];
+            accelerationsz[invi] += diffz * dij * initstate.masses[j];
+
+            accelerationsx[j] -= diffx * dij * initstate.masses[invi];
+            accelerationsy[j] -= diffy * dij * initstate.masses[invi];
+            accelerationsz[j] -= diffz * dij * initstate.masses[invi];
+        }
+    }
+
+#pragma omp parallel for
+    for (int i = 0; i < n_particles; i++)
+    {
         velocitiesx[i] += accelerationsx[i] * 2.0f;
         velocitiesy[i] += accelerationsy[i] * 2.0f;
         velocitiesz[i] += accelerationsz[i] * 2.0f;

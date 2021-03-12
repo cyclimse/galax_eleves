46548d9736f6c367aba94477022526bfe66466ed
Andy Mery <andy.mery@imt-atlantique.net>
2021-03-12 16:35:24 +0100

    First loop with simd

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -3,8 +3,17 @@
 #include "Model_CPU_fast.hpp"
 
 #include <cmath>
+#include <algorithm>
 
+#include <eve/function/sub.hpp>
 #include <eve/function/rsqrt.hpp>
+#include <eve/constant/one.hpp>
+#include <eve/function/hypot.hpp>
+#include <eve/function/max.hpp>
+#include <eve/function/mul.hpp>
+#include <eve/function/div.hpp>
+#include <eve/function/reduce.hpp>
+#include <eve/wide.hpp>
 #include <omp.h>
 
 Model_CPU_fast ::Model_CPU_fast(const Initstate &initstate, Particles &particles)
@@ -24,8 +33,54 @@ void Model_CPU_fast ::step()
 #pragma omp parallel for
     for (int i = 0; i < n_particles / 2; i++)
     {
-        for (int j = 0; j < i; j++)
+        eve::wide<float, eve::fixed<8L>> regis_x;
+        eve::wide<float, eve::fixed<8L>> regis_y;
+        eve::wide<float, eve::fixed<8L>> regis_z;
+        eve::wide<float, eve::fixed<8L>> regis_dij;
+        eve::wide<float, eve::fixed<8L>> regis_m;
+
+        for (int j = 0; j < i - i % 8; j += 8)
         {
+            std::copy(particles.x.cbegin() + j, particles.x.cbegin() + j + 8, regis_x.begin());
+            std::copy(particles.y.cbegin() + j, particles.y.cbegin() + j + 8, regis_y.begin());
+            std::copy(particles.z.cbegin() + j, particles.z.cbegin() + j + 8, regis_z.begin());
+
+            regis_x -= particles.x[i] * eve::one(as(regis_x));
+            regis_y -= particles.y[i] * eve::one(as(regis_y));
+            regis_z -= particles.z[i] * eve::one(as(regis_z));
+
+            regis_dij = eve::hypot(regis_x, regis_y, regis_z);
+            regis_dij = eve::max(1.0f, regis_dij);
+
+            regis_dij = eve::mul(eve::mul(regis_dij, regis_dij), regis_dij);
+            regis_dij = eve::div(10.0f, regis_dij);
+
+            std::copy(initstate.masses.cbegin() + j, initstate.masses.cbegin() + j + 8, regis_m.begin());
+
+            regis_x = eve::mul(regis_x, regis_dij);
+            regis_y = eve::mul(regis_y, regis_dij);
+            regis_z = eve::mul(regis_z, regis_dij);
+
+            accelerationsx[i] += eve::reduce(eve::mul(regis_x, regis_m), std::plus<>{});
+            accelerationsy[i] += eve::reduce(eve::mul(regis_y, regis_m), std::plus<>{});
+            accelerationsz[i] += eve::reduce(eve::mul(regis_z, regis_m), std::plus<>{});
+
+            std::copy(accelerationsx.cbegin() + j, accelerationsx.cbegin() + j + 8, regis_m.begin());
+            regis_m -= initstate.masses[i] * regis_x;
+            std::copy(regis_m.begin(), regis_m.begin() + 8, accelerationsx.begin() + j);
+
+            std::copy(accelerationsy.cbegin() + j, accelerationsy.cbegin() + j + 8, regis_m.begin());
+            regis_m -= initstate.masses[i] * regis_y;
+            std::copy(regis_m.begin(), regis_m.begin() + 8, accelerationsy.begin() + j);
+
+            std::copy(accelerationsz.cbegin() + j, accelerationsz.cbegin() + j + 8, regis_m.begin());
+            regis_m -= initstate.masses[i] * regis_z;
+            std::copy(regis_m.begin(), regis_m.begin() + 8, accelerationsz.begin() + j);
+        }
+
+        for (int j = i - i % 8; j < i; j++)
+        {
+
             const float diffx = particles.x[j] - particles.x[i];
             const float diffy = particles.y[j] - particles.y[i];
             const float diffz = particles.z[j] - particles.z[i];
